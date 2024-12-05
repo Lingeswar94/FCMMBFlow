1d5b8565f1f222318f3c9a9e490c6df37e3c6194
Lingeshwar <Lingeshwar@LAPTOP-TCIABFM5>
2024-12-05 12:50:22 +0530

    Project develope by Java Selenium for MMB Login function

---
+++ FCMMBFlow/src/test/java/UtilityFunction/ExcelData.java
@@ -0,0 +1,37 @@
+package UtilityFunction;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelData {
+
+	 XSSFWorkbook workbook ;
+
+	public ExcelData() {
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream("./src/test/resources/Testdata/TestData.xlsx");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			workbook = new XSSFWorkbook(fileInputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public  String getMMBData(String Sheetname, int Row, int column) {
+		XSSFCell cell = workbook.getSheet(Sheetname).getRow(Row).getCell(column);
+		DataFormatter dataFormatter = new DataFormatter();
+		String data = dataFormatter.formatCellValue(cell);
+		return data;
+	}
+}
